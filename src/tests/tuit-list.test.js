b604539acffdf5bf1623cd319f0754284e0ff42b
sounakbh <sounakbh@gmail.com>
2022-03-15 21:47:58 -0400

    Commpleted Tuits UI Tests

--- src/tests/tuit-list.test.js
+++ src/tests/tuit-list.test.js
@@ -3,8 +3,9 @@ import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {findAllTuits} from "../services/tuits-service";
 import axios from "axios";
+import TuitsData from "../components/tuits/tuits-data.json";
+import TuitsList from "../components/tuits/index";
 
-jest.mock('axios');
 
 const MOCKED_USERS = [
   "alice", "bob", "charlie"
@@ -15,13 +16,36 @@ const MOCKED_TUITS = [
 ];
 
 test('tuit list renders static tuit array', () => {
-  // TODO: implement this
+  render(
+      <HashRouter>
+        <TuitsList tuits={TuitsData}/>
+      </HashRouter>);
+  const linkElement = screen.getByText(/@SpaceX Dragon spacecraft/i);
+  expect(linkElement).toBeInTheDocument();
 });
 
 test('tuit list renders async', async () => {
-  // TODO: implement this
+    const tuits = await findAllTuits();
+    render(
+        <HashRouter>
+            <TuitsList tuits={tuits}/>
+        </HashRouter>);
+    const linkElement = screen.getByText(/sounak's first tuit/i);
+    expect(linkElement).toBeInTheDocument();
 })
 
 test('tuit list renders mocked', async () => {
-  // TODO: implement this
+    const mock = jest.spyOn(axios, 'get');
+    mock.mockImplementation(() =>
+        Promise.resolve({ data: {tuits: TuitsData} }));
+    const response = await findAllTuits();
+    const tuits = response.tuits;
+    render(
+        <HashRouter>
+            <TuitsList tuits={tuits}/>
+        </HashRouter>);
+
+    const user = screen.getByText(/@SpaceX Dragon spacecraft/i);
+    expect(user).toBeInTheDocument();
+    mock.mockRestore();
 });
